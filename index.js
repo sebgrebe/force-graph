dc28926d886b89aeb6c122e3307998a22b6b20d2
sebgrebe <sebbecker@gmx.net>
2017-10-01 17:52:32 +0100

    tidy up code

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ $(document).ready(function() {
    success: (data) => {
      const nodes = data['nodes']
      const links = data['links']
-     var width = 1000, height = 800
+     var width = 1000 
+     var height = 800
 
      var simulation = d3.forceSimulation(nodes)
      .force('charge', d3.forceManyBody().strength(-10))
      .force('center', d3.forceCenter(width / 2, height / 2))
      .force('link', d3.forceLink().links(links).distance(50))
-
      .on('tick', ticked);
 
      function keepInWidth(x) {
@@ -29,57 +29,50 @@ $(document).ready(function() {
             else {return y}
           }  
 
-        function updateLinks() {
-          var u = d3.select('.links')
-          .selectAll('line')
-          .data(links)
+      function updateLinks() {
+        var u = d3.select('.links')
+                  .selectAll('line')
+                  .data(links)
 
-          u.enter()
+        u.enter()
           .append('line')
           .merge(u)
-          .attr('x1', function(d) {
+          .attr('x1', (d) => {
             return keepInWidth(d.source.x)
-            // return d.source.x
           })
-          .attr('y1', function(d) {
+          .attr('y1', (d) => {
             return keepInHeight(d.source.y)
-            // return d.source.y
           })
-          .attr('x2', function(d) {
+          .attr('x2', (d) => {
             return keepInWidth(d.target.x)
-            // return d.target.x
           })
-          .attr('y2', function(d) {
+          .attr('y2', (d) => {
             return keepInHeight(d.target.y)
-            // return d.target.y
           })
-
           u.exit().remove()
-        }
+      }
 
-        function findNeighbour(index) {
-
-          var neighbours = [];
-
-          for (var i=0;i<links.length;i++) {
-            var source_index = links[i]['source']['index'];
-            var target_index = links[i]['target']['index'];
-            if (source_index === index) {
-              neighbours.push(nodes[target_index]['code']);
-            }
-            else if (target_index === index) {
-              neighbours.push(nodes[source_index]['code'])
-            }
+      function findNeighbour(index) {
+        var neighbours = [];
+        for (var i=0;i<links.length;i++) {
+          var source_index = links[i]['source']['index'];
+          var target_index = links[i]['target']['index'];
+          if (source_index === index) {
+            neighbours.push(nodes[target_index]['code']);
           }
-          return neighbours
+          else if (target_index === index) {
+            neighbours.push(nodes[source_index]['code'])
+          }
         }
+        return neighbours
+      }
 
-        function updateNodes() {
-          var u = d3.select('.nodes')
-          .selectAll('image')
-          .data(nodes)
+      function updateNodes() {
+        var u = d3.select('.nodes')
+        .selectAll('image')
+        .data(nodes)
 
-          u.enter()
+        u.enter()
           .append('svg:image')
           .merge(u)
           .attr('x', (d) => keepInWidth(d.x)-8)
@@ -87,64 +80,62 @@ $(document).ready(function() {
           .attr('width',20)
           .attr('height',14)
           .attr('xlink:href', (d) => 'flags/'+d['code']+'.png')
-           .exit().remove()
-        }
-
-function updateRect() {
-  var u = d3.select('.nodes')
-      .selectAll('rect')
-            .data(nodes)
-          
-          u.enter()
-          .append('svg:rect')
-          .merge(u)
-          .attr('x', (d) => keepInWidth(d.x)-8)
-          // .attr('x', (d) => d.x-8)
-          .attr('y', (d) => keepInHeight(d.y)-11)
-          // .attr('y', (d) => d.y-11)
-          .attr('width',20)
-          .attr('height',14)
-          .attr('stroke','none')
-          .attr('stroke-width',2)
-          .attr('fill','none')
-          .attr('class',(d) => d['code'])
           .exit().remove()
       }
 
-//highlight neighbours and show tooltip
-function highlight() {
-  var u = d3.select('.nodes')
-  u.selectAll('image')
-  .data(nodes)
-  .on('mouseover',(d,i) => {
-    u.append('title')
-    .attr('x', keepInWidth(d.x)+10)
-    .attr('y', keepInHeight(d.y)-11)
-    .attr('width',80)
-    .attr('height',40)
-    .text(d['country'])
+        function updateRect() {
+        var u = d3.select('.nodes')
+                  .selectAll('rect')
+                  .data(nodes)
+              
+                 u.enter()
+                  .append('svg:rect')
+                  .merge(u)
+                  .attr('x', (d) => keepInWidth(d.x)-8)
+                  .attr('y', (d) => keepInHeight(d.y)-11)
+                  .attr('width',20)
+                  .attr('height',14)
+                  .attr('stroke','none')
+                  .attr('stroke-width',2)
+                  .attr('fill','none')
+                  .attr('class',(d) => d['code'])
+                  .exit().remove()
+          }
 
-   var neighbours = findNeighbour(i);
-   for (var j=0;j<neighbours.length;j++) {
-    $('.'+neighbours[j]).addClass('highlight')       
-  }
-})
-  .on('mouseout',(d,i) => {
-    var u = d3.select('.nodes')
-    u.selectAll('title').remove()
-    var neighbours = findNeighbour(i);
-    for (var j=0;j<neighbours.length;j++) {
-      $('.'+neighbours[j]).removeClass('highlight')
-    }
-  })
-}
+        //highlight neighbours and show tooltip
+        function highlight() {
+          var u = d3.select('.nodes')
+          u.selectAll('image')
+            .data(nodes)
+            .on('mouseover',(d,i) => {
+              u.append('title')
+                .attr('x', keepInWidth(d.x)+10)
+                .attr('y', keepInHeight(d.y)-11)
+                .attr('width',80)
+                .attr('height',40)
+                .text(d['country'])
 
-function ticked() {
-  updateLinks()
-  updateNodes()
-  updateRect()
-  highlight()
-}
-}                                  
-});
+              var neighbours = findNeighbour(i);
+              for (var j=0;j<neighbours.length;j++) {
+                $('.'+neighbours[j]).addClass('highlight')       
+              }
+            })
+            .on('mouseout',(d,i) => {
+              var u = d3.select('.nodes')
+              u.selectAll('title').remove()
+              var neighbours = findNeighbour(i);
+              for (var j=0;j<neighbours.length;j++) {
+                $('.'+neighbours[j]).removeClass('highlight')
+              }
+            })
+          }
+
+        function ticked() {
+          updateLinks()
+          updateNodes()
+          updateRect()
+          highlight()
+        }
+      }                                  
+    });
 })
